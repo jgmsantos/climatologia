cadb6a76de6a6cfa1f5b5c07f5e2562f5fd46408
Guilherme <jgmsantos@gmail.com>
2025-02-26 15:07:08 -0300

    Primeiro commit

--- scripts/limpa.sh
+++ scripts/limpa.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 diretorio_dados="/mnt/c/Users/guilherme.martins/guilherme/base_dados/FOCOS/INPE/satelite_referencia/netcdf/mensal"
-diretorio_tmp="/mnt/c/Users/guilherme.martins/guilherme/streamlit/climatologia_focos/tmp"
-diretorio_output="/mnt/c/Users/guilherme.martins/guilherme/streamlit/climatologia_focos/output"
+diretorio_tmp="/mnt/c/Users/guilherme.martins/guilherme/streamlit/climatologia/tmp"
+diretorio_output="/mnt/c/Users/guilherme.martins/guilherme/streamlit/climatologia/output"
 
-cdo -s --no_history -ymonmean -selyear,2003/2024 -mergetime ${diretorio_dados}/*.nc ${diretorio_tmp}/climatologia_espacial.nc
+cdo -s --no_history -ymonmean -selyear,2003/2024 -mergetime ${diretorio_dados}/*.nc ${diretorio_output}/clima_focos_espacial.nc
